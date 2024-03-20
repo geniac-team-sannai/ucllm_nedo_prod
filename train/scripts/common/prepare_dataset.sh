854e4f1a7a959d66edd874f481e73fd7e17279fb
issei <issei@ifmac.local>
2024-03-20 17:16:09 +0900

    add prepare_dataset

---
+++ train/scripts/common/prepare_dataset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+input=${megatron_deepspeed_dir}"/dataset/wiki.jsonl"
+output_prefix=${megatron_deepspeed_dir}"/dataset/wiki"
+megatron_deepspeed_dir="/home/ext_otomijuf_004_gmail_com/Megatron-DeepSpeed"
+tokenizer="llamaste/Llama-2-7b-chat-hf"
+
+
+python ${megatron_deepspeed_dir}/tools/preprocess_data.py \
+    --tokenizer-type HFTokenizer \
+    --tokenizer-model ${tokenizer} \
+    --input ${input} \
+    --output-prefix ${output_prefix} \
+    --dataset-impl mmap \
+    --workers $(grep -c ^processor /proc/cpuinfo) \
+    --append-eod
