84789b00dc3f0df7691201898828c2d35d621b62
issei <issei@ifmac.local>
2024-03-20 22:55:05 +0900

    for sample dataset

--- train/scripts/common/prepare_dataset.sh
+++ train/scripts/common/prepare_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-input=${megatron_deepspeed_dir}"/dataset/wiki.jsonl"
+jsonl_file=${megatron_deepspeed_dir}"/dataset/wiki.jsonl"
 output_prefix=${megatron_deepspeed_dir}"/dataset/wiki"
 megatron_deepspeed_dir="/home/ext_otomijuf_004_gmail_com/Megatron-DeepSpeed"
 tokenizer="llamaste/Llama-2-7b-chat-hf"
@@ -9,7 +9,7 @@ tokenizer="llamaste/Llama-2-7b-chat-hf"
 python ${megatron_deepspeed_dir}/tools/preprocess_data.py \
     --tokenizer-type HFTokenizer \
     --tokenizer-model ${tokenizer} \
-    --input ${input} \
+    --input ${jsonl_file} \
     --output-prefix ${output_prefix} \
     --dataset-impl mmap \
     --workers $(grep -c ^processor /proc/cpuinfo) \
