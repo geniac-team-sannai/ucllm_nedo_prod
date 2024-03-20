4d7f24ba36f36aa791799ec76fe7d06ee332c27f
issei <issei@ifmac.local>
2024-03-20 23:49:24 +0900

    fix: use phi2 tokenizer

--- train/scripts/step2_pretrain_model/gcp_node-1_gpu/wiki-en_gpt_tiny/prepare_dataset.sh
+++ train/scripts/step2_pretrain_model/gcp_node-1_gpu/wiki-en_gpt_tiny/prepare_dataset.sh
@@ -3,7 +3,7 @@
 jsonl_file="./train.jsonl"
 output_prefix=${megatron_deepspeed_dir}"/dataset/wiki"
 megatron_deepspeed_dir="/home/ext_otomijuf_004_gmail_com/Megatron-DeepSpeed"
-tokenizer="llamaste/Llama-2-7b-chat-hf"
+tokenizer="microsoft/phi-2"
 
 python ${megatron_deepspeed_dir}/tools/preprocess_data.py \
     --tokenizer-type HFTokenizer \
