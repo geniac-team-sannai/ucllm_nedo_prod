61ba5e32e0b4a5c2cda92e1df2e8ea930afe895b
issei <issei@ifmac.local>
2024-03-21 00:15:27 +0900

    fix

--- train/scripts/step2_pretrain_model/gcp_node-1_gpu/wiki-en_gpt_tiny/download_hf_ds.py
+++ train/scripts/step2_pretrain_model/gcp_node-1_gpu/wiki-en_gpt_tiny/download_hf_ds.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
-dataset_name = "izumi-lab/wikinews-ja-20230728"
+dataset_name = "izumi-lab/wikinews-en-20230728"
 dataset = load_dataset(dataset_name)
 dataset['train'].to_json("./train.jsonl", orient="records", lines=True,  force_ascii=False)
