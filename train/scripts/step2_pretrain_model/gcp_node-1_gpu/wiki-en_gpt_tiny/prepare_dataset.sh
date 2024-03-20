d753466a9ad1de7c606fcb43d697cfd8aa375bd9
issei <issei@ifmac.local>
2024-03-20 23:20:29 +0900

    fix

--- train/scripts/step2_pretrain_model/gcp_node-1_gpu/wiki-en_gpt_tiny/prepare_dataset.sh
+++ train/scripts/step2_pretrain_model/gcp_node-1_gpu/wiki-en_gpt_tiny/prepare_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jsonl_file="./train.jsonl"
-output_prefix="./dataset/wiki"
+output_prefix=${megatron_deepspeed_dir}"/dataset/wiki"
 megatron_deepspeed_dir="/home/ext_otomijuf_004_gmail_com/Megatron-DeepSpeed"
 tokenizer="llamaste/Llama-2-7b-chat-hf"
 
